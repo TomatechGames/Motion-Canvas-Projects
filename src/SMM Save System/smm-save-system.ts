fd653c05bcf23f8d3fe9dbdbde580d454f0acb16
TomatechGames <theludwiggamer@gmail.com>
2023-03-07 00:22:57 +0000

    Renamed scene in SMM Save System

--- src/SMM Save System/smm-save-system.ts
+++ src/SMM Save System/smm-save-system.ts
@@ -1,7 +1,7 @@
 import {makeProject} from '@motion-canvas/core';
 
-import example from './scenes/example?scene';
+import tiledata from './scenes/tiledata?scene';
 
 export default makeProject({
-  scenes: [example]
+  scenes: [tiledata]
 });
