970cd26a5aabbfe172fcdc599286729a14bcca96
TomatechGames <theludwiggamer@gmail.com>
2023-03-11 15:22:11 +0000

    Imported custom font

--- src/shared/components/IntroComponent/index.tsx
+++ src/shared/components/IntroComponent/index.tsx
@@ -3,7 +3,9 @@ import { delay, noop, waitFor } from "@motion-canvas/core/lib/flow";
 import { easeInCubic, easeInOutCubic } from "@motion-canvas/core/lib/tweening";
 import { Vector2 } from "@motion-canvas/core/lib/types";
 import { createRef } from "@motion-canvas/core/lib/utils";
+
 import miniIntro from './miniIntro.webm';
+import '../../fonts.css';
 
 
 export interface IntroComponentProps extends LayoutProps {
@@ -28,7 +30,7 @@ export class IntroComponent extends Layout {
             <>
                 <Circle ref={this.circleRef} size={()=>Math.sqrt((this.size.x()*this.size.x())+(this.size.y()*this.size.y()))+5} fill={'#141414'}/>
                 <Video ref={this.vidRef} src={miniIntro} scale={0.25}>
-                    <Txt ref={this.textRef} text={'Tomatech'} opacity={0} fontFamily={'MARIO MAKER'} offset={[-1,1]} scale={10} fontSize={96} position={()=>this.vidRef().size().scale(0.5).add(new Vector2(0,80))} fill={'#800'}/>
+                    <Txt ref={this.textRef} text={'Tomatech'} opacity={0} fontFamily={'Super Mario Maker'} offset={[-1,1]} scale={10} fontSize={96} position={()=>this.vidRef().size().scale(0.5).add(new Vector2(0,80))} fill={'#800'}/>
                 </Video>
             </>
         );
